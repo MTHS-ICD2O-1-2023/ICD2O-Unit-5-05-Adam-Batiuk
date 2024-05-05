058026a92ed5a45062e0a1eb42c83a006bb1253a
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-05 22:30:04 +0000

    done

--- js/script.js
+++ js/script.js
@@ -7,21 +7,27 @@
 
 function myButtonClicked() {
   // get inputs
-  const lengthA = parseInt(document.getElementById("side-a").value)
-  const lengthB = parseInt(document.getElementById("side-b").value)
-  const lengthC = parseInt(document.getElementById("side-c").value)
+  const lengthA = parseFloat(document.getElementById("side-a").value)
+  const lengthB = parseFloat(document.getElementById("side-b").value)
+  const lengthC = parseFloat(document.getElementById("side-c").value)
 
   // using the cosine law
   const angleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)) * (180 / Math.PI)
   const angleB = Math.acos((lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)) * (180 / Math.PI)
   const angleC = Math.acos((lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)) * (180 / Math.PI)
+  
+  //console.log("angleA,B,C  " + angleA + "  " + angleB + "  " + angleC)
 
   const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+  
+  //console.log("Sum of angles  " + sumOfAngles)
   if (sumOfAngles != 180) {
     document.getElementById("answer").innerHTML = "These inputs do not make a triangle."
-  }
-  // determen type
-  if (((angleA == angleC) && (angleA != angleB)) || ((angleA == angleB) && (angleA != angleC)) || ((angleB == angleC) && (angleB != angleA))) {
-    document.getElementById("answer").innerHTML = "test"
+  } else if (((angleA == angleC) && (angleA != angleB)) || ((angleA == angleB) && (angleA != angleC)) || ((angleB == angleC) && (angleB != angleA))) {
+    document.getElementById("answer").innerHTML = "This is a Isosceles triangle."
+  } else if ((angleA != angleB) && (angleA != angleC) && (angleB != angleC)) {
+    document.getElementById("answer").innerHTML = "This is a Scalene Triangle triangle."
+  } else if ((angleA == angleB) && (angleA == angleC)) {
+    document.getElementById("answer").innerHTML = "This is a Equilateral Triangle triangle."
   }
 }
